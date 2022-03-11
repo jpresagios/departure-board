487179cbe7c1fba7339a80296eb220e8357fc928
Junior Paz Formoso <junior.presagios@gmail.com>
2022-03-12 01:54:03 +0200

    Added Snapshot for BoardEntry component

--- src/tests/BoardEntry.test.tsx
+++ src/tests/BoardEntry.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+// import { render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import BoardEntry from '../components/BoardEntry';
 import fakePredictions from './fakePredictions';
 
 test('Board Entry renders correctly', () => {
-  const tree = renderer.create(<BoardEntry data={fakePredictions[0]} />).toJSON();
+  const tree = renderer.create(<BoardEntry data={fakePredictions[0]} isArrival />).toJSON();
   expect(tree).toMatchSnapshot();
 });
