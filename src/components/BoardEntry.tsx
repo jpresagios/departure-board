36d09c5a20f5b18a62404750f19342926e6b3f3f
Junior Paz Formoso <junior.presagios@gmail.com>
2022-03-11 20:48:11 +0200

    Added BoardEntry component

---
+++ src/components/BoardEntry.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import { IPrediction } from "../dataSource/IPredition";
+import { getCarrier, getDestinationName, getTrainNumber, getTrainStatus, getTrainTime, getTrainTrackNumber } from "../utils/boardData";
+
+
+/**
+ * This component represent an entry on the board (Departure or Arrival)
+ * @param props Contains a prediction from MTBA API
+ * @returns 
+ */
+function BoardEntry(props: { data: IPrediction }) {
+  const { data } = props;
+  const [destinationName, setDestinationName] = useState("");
+
+
+  let trainNumber = getTrainNumber(data);
+  const trainTrackNumber = getTrainTrackNumber(data);
+  const status = getTrainStatus(data);
+
+  const {
+    relationships: { trip: { data: { id } } },
+  } = data;
+
+  useEffect(() => {
+    getDestinationName(id).then(destination => {
+      setDestinationName(destination)
+    })
+  }, [id])
+
+  return (
+    <TableRow>
+      <TableCell align="right">{getCarrier(data)}</TableCell>
+      <TableCell align="right">{getTrainTime(data)}</TableCell>
+      <TableCell align="right">{destinationName}</TableCell>
+      <TableCell align="right">{trainNumber}</TableCell>
+      <TableCell align="right">{trainTrackNumber}</TableCell>
+      <TableCell align="right">{status}</TableCell>
+    </TableRow>
+  );
+}
+
+export default BoardEntry;
