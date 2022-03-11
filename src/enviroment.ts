58b4143c1051e22fc120987c65dffcbbb1498313
Junior Paz Formoso <junior.presagios@gmail.com>
2022-03-11 22:42:49 +0200

    Update API MTBA key

--- src/enviroment.ts
+++ src/enviroment.ts
@@ -1,3 +1,3 @@
-const apiKey: string = process.env.REACT_APP_APIKEY as string;
+const apiKey: string = process.env.REACT_APP_MBTA_APIKEY as string;
 
-export default apiKey || 'casa';
+export default apiKey || '8bc7cddc9a404daf8a3d40e098a9f980';
