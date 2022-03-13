58385727a89dcd1574f1bf4b06eb09f26a26f381
Junior Paz Formoso <junior.presagios@gmail.com>
2022-03-13 18:39:29 +0200

    Change logic to request data

--- src/utils/boardData.ts
+++ src/utils/boardData.ts
@@ -3,61 +3,28 @@
  */
 
 import axios from 'axios';
-import { IPrediction } from '../dataSource/IPredition';
+import * as logger from 'loglevel';
+import { IPrediction, ITrainsBoard } from '../dataSource/IPredition';
 import apiKey from '../enviroment';
 
 const getCarrier = () => 'MTBA';
 
-const getTrainTime = (prediction: IPrediction, isArrival: boolean) => {
-  let time = null;
-
+const getTrainTime = (prediction: IPrediction, isArrival: boolean | null) => {
   if (isArrival) {
     const {
       // eslint-disable-next-line @typescript-eslint/naming-convention
       attributes: { arrival_time }
     } = prediction;
-    time = arrival_time;
-  } else {
-    const {
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      attributes: { departure_time }
-    } = prediction;
 
-    time = departure_time;
+    return new Date(arrival_time!).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
   }
-
-  const realHour: number = new Date(time!).getHours();
-  const realMinutes: number = new Date(time!).getMinutes();
-  let suffix = 'AM';
-
-  let displayHour: string = `${realHour}`;
-  let displayMinutes: string = `${realMinutes}`;
-
-  if (realHour > 11) {
-    displayHour = `${realHour - 12}`;
-    suffix = 'PM';
-  }
-  if (realHour === 0) {
-    displayHour = '12';
-  }
-  if (realMinutes < 10) {
-    displayMinutes = `0${realMinutes}`;
-  }
-  return `${displayHour}:${displayMinutes} ${suffix}`;
-};
-
-const getDestinationName = async (tripId: string): Promise<string> => {
-  const tripData = await axios.get(
-    `https://api-v3.mbta.com/trips/${tripId}?api_key=${apiKey}`
-  );
 
   const {
-    data: {
-      attributes: { headsign }
-    }
-  } = tripData.data;
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    attributes: { departure_time }
+  } = prediction;
 
-  return headsign;
+  return new Date(departure_time!).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 };
 
 const getTrainNumber = (prediction: IPrediction) => {
@@ -65,8 +32,9 @@ const getTrainNumber = (prediction: IPrediction) => {
     relationships: { vehicle }
   } = prediction;
 
-  const vehicleData = vehicle.data.id.split('-');
-  return vehicleData.length > 1 ? vehicleData[1] : vehicleData;
+  if (vehicle.data === null) return '';
+  if (vehicle.data.id.substring(0, 5) === 'block') return vehicle.data.id.substring(6, 10);
+  return vehicle.data.id;
 };
 
 const getTrainTrackNumber = (prediction: IPrediction) => {
@@ -74,8 +42,10 @@ const getTrainTrackNumber = (prediction: IPrediction) => {
     relationships: { stop }
   } = prediction;
 
+  if (stop.data === null) return '';
+
   const trackData = stop.data.id.split('-');
-  return trackData.length > 1 ? stop.data.id[1] : 'TBD';
+  return trackData.length > 1 ? trackData[0] : 'TBD';
 };
 
 const getTrainStatus = (prediction: IPrediction) => {
@@ -85,11 +55,102 @@ const getTrainStatus = (prediction: IPrediction) => {
   return status;
 };
 
+// eslint-disable-next-line max-len
+const getDepartures = (data: IPrediction[], tripsData: Map<string, string>): IPrediction[] => {
+  const departuresResult = [];
+
+  const validDepartures = (pred: IPrediction) => {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const { attributes: { stop_sequence, departure_time } } = pred;
+    return (stop_sequence === 1
+      && departure_time !== null
+      && new Date(departure_time).getTime() > new Date().getTime())
+      || (departure_time !== null && stop_sequence === 0);
+  };
+
+  const departures = data.filter((pred) => validDepartures(pred));
+
+  for (let i = 0; i < departures.length; i += 1) {
+    const departureResult = { ...departures[i] };
+
+    const {
+      relationships: { trip }
+    } = departures[i];
+
+    departureResult.destination = tripsData.get(trip.data.id);
+    departuresResult.push(departureResult);
+  }
+
+  const sortedDepartures = departuresResult.sort(
+    (a: IPrediction, b: IPrediction) => new Date(a.attributes.departure_time!).getTime()
+      - new Date(b.attributes.departure_time!).getTime()
+  );
+
+  return sortedDepartures;
+};
+
+// eslint-disable-next-line max-len
+const getArrivals = (data: IPrediction[], tripsData: Map<string, string>): IPrediction[] => {
+  const arrivalsResult = [];
+
+  const arrivals = data.filter(
+    (pred) => pred.attributes.stop_sequence > 1 && pred.attributes.arrival_time !== null
+  );
+
+  for (let i = 0; i < arrivals.length; i += 1) {
+    const {
+      relationships: { trip }
+    } = arrivals[i];
+
+    if (trip.data.id) {
+      const destinationName = tripsData.get(trip.data.id);
+      if (destinationName === 'South Station') {
+        arrivals[i].destination = destinationName;
+        arrivalsResult.push(arrivals[i]);
+      }
+    }
+  }
+
+  const sortedArrivals = arrivalsResult.sort(
+    (a: IPrediction, b: IPrediction) => new Date(a.attributes.arrival_time!).getTime()
+      - new Date(b.attributes.arrival_time!).getTime()
+  );
+
+  return sortedArrivals;
+};
+
+const getTrainsInfo = async (dataPredictions: IPrediction[]): Promise<ITrainsBoard> => {
+  const tripDataResult = new Map<string, string>();
+  const tripsId = dataPredictions.filter((pred) => pred.relationships.trip.data !== null)
+    .map((pred) => pred.relationships.trip.data.id).join(',');
+
+  try {
+    const tripData = await axios.get(`https://api-v3.mbta.com/trips/?api_key=${apiKey}&id=${tripsId}`);
+    const { data: { data } } = tripData;
+
+    for (let i = 0; i < data.length; i += 1) {
+      const { attributes: { headsign }, id } = data[i];
+      tripDataResult.set(id, headsign);
+    }
+
+    const departuresData = getDepartures(dataPredictions, tripDataResult);
+    const arrivalsData = getArrivals(dataPredictions, tripDataResult);
+
+    return { departuresData, arrivalsData };
+  } catch (err) {
+    logger.error('Resolve trains info:', err);
+  }
+
+  return { departuresData: [], arrivalsData: [] };
+};
+
 export {
+  getTrainsInfo,
+  getDepartures,
+  getArrivals,
   getCarrier,
   getTrainTime,
-  getDestinationName,
   getTrainNumber,
   getTrainTrackNumber,
-  getTrainStatus
+  getTrainStatus,
 };
